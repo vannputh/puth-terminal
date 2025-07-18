6ce30e3046287bb4570bc5d33f6ddbb0e619d55f
Daroh Sou <daroh.sou22@kit.edu.kh>
2025-07-18 16:34:26 +0700

    moving commands to terminal from header, fixed globe

--- src/components/Globe.tsx
+++ src/components/Globe.tsx
@@ -18,15 +18,10 @@ const Globe: React.FC<GlobeProps> = ({ isOpen, onClose }) => {
     >
       <div className="w-4/5 h-4/5 bg-gray-900 border-2 border-gray-700 rounded-lg shadow-lg relative">
         <div className="absolute top-0 left-0 right-0 bg-gray-800 p-2 flex items-center justify-between z-20">
-          <div className="flex items-center">
-            <div className="w-3 h-3 bg-red-500 rounded-full mx-1"></div>
-            <div className="w-3 h-3 bg-yellow-500 rounded-full mx-1"></div>
-            <div className="w-3 h-3 bg-green-500 rounded-full mx-1"></div>
-            <div className="text-gray-300 text-sm ml-4">Globe Viewer</div>
-          </div>
+          <div className="flex-grow text-center text-gray-300 text-sm">Globe Viewer</div>
           <button
             onClick={onClose}
-            className="text-white font-bold hover:text-gray-400"
+            className="text-white font-bold hover:text-gray-400 pr-2"
           >
             &times;
           </button>
