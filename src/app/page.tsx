57ba96663b817926dd217947a3fa8fc63540a7af
Daroh Sou <daroh.sou22@kit.edu.kh>
2025-07-22 00:11:56 +0700

    added status bar and changed the display of the terminal outputs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import Header from "@/components/Header";
 import Terminal from "@/components/Terminal";
+import StatusBar from "@/components/StatusBar";
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center min-h-screen p-4 font-mono sm:p-8">
-      <Terminal />
+    <main className="flex flex-col h-screen bg-black text-white font-mono">
+      <Header />
+      <div className="flex-1 p-4 overflow-hidden">
+        <Terminal />
+      </div>
+      <StatusBar />
     </main>
   );
 }
