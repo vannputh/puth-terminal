971270131a544e9ed64d194f87bd7bd23a1b3dd7
Daroh Sou <daroh.sou22@kit.edu.kh>
2025-07-22 13:27:00 +0700

    added the css for mobile view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import StatusBar from "@/components/StatusBar";
 
 export default function Home() {
   return (
-    <main className="flex flex-col h-screen bg-black text-white font-mono">
+    <main className="flex flex-col h-screen bg-black text-white font-mono pb-6 md:pb-5">
       <Header />
-      <div className="flex-1 p-4 overflow-hidden">
+      <div className="flex-1 p-2 md:p-4 overflow-hidden">
         <Terminal />
       </div>
       <StatusBar />
